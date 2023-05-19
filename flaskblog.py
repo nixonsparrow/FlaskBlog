00f81a405272813e6e768d7a81dd27b32e315666
Nikodem Wróbel <nwrobel@bsbox.pl>
2023-05-19 15:57:45 +0200

    Add smart templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,8 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Nikodem Wróbel",
+        "title": "Blog post 1",
+        "content": "This is the first blog post",
+        "date_posted": "May 19, 2023"
+    },
+    {
+        "author": "Nikodem Wróbel",
+        "title": "Blog post 2",
+        "content": "This is the second blog post",
+        "date_posted": "May 20, 2023"
+    }
+]
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home_page():
+    return render_template("home.html", posts=posts)
+
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
